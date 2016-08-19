3a8bcde792c7e854faef0b1652c58c1672470bd4
Chad Ostrowski <hi@chadoh.com>
2016-08-19 08:36:59 -0400

    Switch back to browserHistory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { hashHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 import { loadState, saveState } from './helpers/localStorage';
 import Root from './components/Root';
 import createStore from './redux/create';
@@ -14,6 +14,6 @@ store.subscribe(() => {
 });
 
 ReactDOM.render(
-  <Root store={store} history={hashHistory} />,
+  <Root store={store} history={browserHistory} />,
   document.getElementById('root')
 );
