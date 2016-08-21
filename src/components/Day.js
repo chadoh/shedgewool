4737add8a27effae541097cc7aca1655c6e0d8fb
Chad Ostrowski <hi@chadoh.com>
2016-08-20 20:20:35 -0400

    Slide instead of popping; scroll to shown session

--- src/components/Day.js
+++ src/components/Day.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router';
 import { load as loadSchedule } from '../redux/reducers/schedule';
 import Loader from './Loader';
 import Container from './Container';
+import DetailContainer from './DetailContainer';
+import Session from './Session';
 import '../styles/Day.css';
 
 const contains = (sessions, id) => {
@@ -16,6 +18,17 @@ class Day extends Component {
     favorites: React.PropTypes.object.isRequired,
   }
 
+  state = {
+    lastChildId: null,
+  }
+
+  componentWillReceiveProps(newProps) {
+    const oldSessionId = this.props.params.sessionId;
+    if (newProps.params.sessionId !== oldSessionId) {
+      this.setState({ lastChildId: oldSessionId });
+    }
+  }
+
   componentDidMount() {
     const { schedule } = this.props;
     if (!(schedule.data || schedule.error) && !schedule.loading) {
@@ -23,8 +36,69 @@ class Day extends Component {
     }
   }
 
+  renderTimeslot = (hour) => {
+    const { schedule, favorites, params: {day, sessionId: currentId} } = this.props;
+    const {lastChildId} = this.state;
+    const currentSession = contains(schedule.data[day][hour], currentId);
+    const lastSession = contains(schedule.data[day][hour], lastChildId);
+    const sameRow = lastSession && currentSession;
+    return (
+      <div key={hour}>
+        <Container className="Day-timeslotWrapper">
+          <div className="Day-timeslot">
+            <div className="Day-hour">{hour}</div>
+            {schedule.data[day][hour].map(session => {
+              return (
+                <div key={session.id} className={
+                  `Day-session ${favorites[session.id] && 'Day-favoriteSession'}`}>
+                  {favorites[session.id] &&
+                    <span className="Day-favorite">⭐️ </span>
+                  }
+                  <Link className={
+                    `Day-sessionLink ${favorites[session.id] && 'Day-favoriteSession'}`}
+                    to={session.id === currentId ? `/${day}` : `/${day}/${session.id}`}>
+                    {session.image &&
+                      <div className="Day-imageWrap">
+                        <div className="Day-image" title={session.talk.title}
+                          style={{backgroundImage: `url(${session.image})`}}/>
+                        <br/>
+                      </div>
+                    }
+                    <span className="Day-sessionTitle">
+                      {session.stage}
+                    </span>
+                    <small className="Day-sessionTime">
+                      {session.time_start} – {session.time_end}
+                    </small>
+                  </Link>
+                  {session.id === currentId &&
+                    <div className="Day-arrowWrap">
+                      <div className="Day-arrow"/>
+                    </div>
+                  }
+                </div>
+              );
+            })}
+          </div>
+        </Container>
+        {currentSession &&
+          this.renderDetail({session: currentSession})}
+        {(lastSession && !sameRow) &&
+          this.renderDetail({session: lastSession, old: true})}
+      </div>
+    );
+  }
+
+  renderDetail = ({session, old}) => {
+    return (
+      <DetailContainer old={old}>
+        <Session {...this.props} session={session}/>
+      </DetailContainer>
+    );
+  }
+
   render() {
-    const { schedule, favorites, params: {day, sessionId} } = this.props;
+    const { schedule, params: {day} } = this.props;
 
     if (!schedule.data) return <Loader />;
 
@@ -40,58 +114,10 @@ class Day extends Component {
           </header>
         </Container>
 
-        {
-          Object.keys(
-            schedule.data[day] || {}
-          ).sort((a, b) => +a - +b).map(hour => {
-          const currentSession = contains(schedule.data[day][hour], sessionId);
-          return (
-            <div key={hour}>
-              <Container className="Day-timeslotWrapper">
-                <div className="Day-timeslot">
-                  <div className="Day-hour">{hour}</div>
-                  {schedule.data[day][hour].map(session => {
-                    return (
-                      <div key={session.id} className={
-                        `Day-session ${favorites[session.id] && 'Day-favoriteSession'}`}>
-                        {favorites[session.id] &&
-                          <span className="Day-favorite">⭐️ </span>
-                        }
-                        <Link className={
-                          `Day-sessionLink ${favorites[session.id] && 'Day-favoriteSession'}`}
-                          to={session.id === sessionId ? `/${day}` : `/${day}/${session.id}`}>
-                          {session.image &&
-                            <div className="Day-imageWrap">
-                              <div className="Day-image" title={session.talk.title}
-                                style={{backgroundImage: `url(${session.image})`}}/>
-                              <br/>
-                            </div>
-                          }
-                          <span className="Day-sessionTitle">
-                            {session.stage}
-                          </span>
-                          <small className="Day-sessionTime">
-                            {session.time_start} – {session.time_end}
-                          </small>
-                        </Link>
-                        {sessionId && session.id === sessionId &&
-                          <div className="Day-arrowWrap">
-                            <div className="Day-arrow"/>
-                          </div>
-                        }
-                      </div>
-                    );
-                  })}
-                </div>
-              </Container>
-              { sessionId && currentSession &&
-                React.cloneElement(this.props.children, {
-                  session: currentSession,
-                })
-              }
-            </div>
-          );
-        })}
+        {Object.keys(schedule.data[day] || {})
+          .sort((a, b) => +a - +b)
+          .map(this.renderTimeslot)
+        }
       </div>
     );
   }
---
+++ src/components/DetailContainer.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+const animationSpeed = 300;
+const transition = `height ${animationSpeed}ms ease`;
+
+export default class DetailContainer extends React.Component {
+  static propTypes = {
+    old: React.PropTypes.bool,
+    children: React.PropTypes.element.isRequired,
+  }
+
+  static defaultProps = {
+    old: false,
+  }
+
+  state = {
+    animate: false,
+    height: this.props.old ? 'auto' : 0,
+    styles: { transition },
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  componentWillReceiveProps() {
+    this.setState({animate: true});
+  }
+
+  componentDidUpdate() {
+    if (this.state.animate) {
+      this.animate();
+      this.setState({animate: false}); // eslint-disable-line react/no-did-update-set-state
+    }
+    if (!this.props.old) this.scrollToTop();
+  }
+
+  scrollToTop() {
+    setTimeout(() => {
+      if (this.refs.top) this.scrollTo(this.refs.top.offsetTop - 155);
+    }, animationSpeed);
+  }
+
+  scrollTo(to) {
+    if (this.alreadyVisible()) return;
+
+    window.scroll(0, to);
+  }
+
+  alreadyVisible() {
+    return this.refs.top.offsetTop > window.pageYOffset &&
+      this.refs.top.offsetTop < window.pageYOffset + window.innerHeight;
+  }
+
+  animate() {
+    if (!this.refs.container) return;
+    const height = this.refs.container.offsetHeight;
+    this.setState({height}, () => {
+      if (this.props.old) {
+        setTimeout(() => { // wait for hard-coded height to be actually set, then animate shrink
+          this.setState({height: 0});
+        }, 1);
+      } else {
+        setTimeout(() => {
+          this.setState({styles: {transition: 'none'}, height: 'auto'});
+          setTimeout(() => {
+            this.setState({styles: {transition}});
+          }, 1);
+        }, animationSpeed);
+      }
+    });
+  }
+
+  render() {
+    const inlineStyles = {
+      ...this.state.styles,
+      height: this.state.height,
+      overflow: 'hidden',
+    };
+    return (
+      <div ref="top" style={inlineStyles}>
+        <div ref="container">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
